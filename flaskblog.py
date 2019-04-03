3b4218650eb3d1a851b042cc68d0e444ac6a523f
prathams7 <prathms007@gmail.com>
2019-04-04 02:21:24 +0530

    change in css and added database

--- flaskblog.py
+++ flaskblog.py
@@ -15,7 +15,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, defaul='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref="authon", lazy=True)
 
